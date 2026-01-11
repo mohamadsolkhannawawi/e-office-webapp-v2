f1a499434d8043baed7f374bfb2c20a3f411cb1d
MohamadSolkhanNawawi <mohamadsolkhan558@gmail.com>
2026-01-11 20:16:36 +0700

    feat(api): add application api client and configure next.js proxy

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,16 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-    //surat-rekomendasi-beasiswa route
+    rewrites: async () => {
+        return {
+            beforeFiles: [
+                {
+                    source: "/api/:path*",
+                    destination: "http://localhost:3005/api/:path*",
+                },
+            ],
+        };
+    },
 };
 
 export default nextConfig;

fae83366df2a000d70bcb19c926109e00e3a8561
MOHAMMAD IMRON ROSYADI <imronrsya@gmail.com>
2026-01-12 10:43:03 +0700

    fix validation email

--- src/utils/validations/suratRekomendasi.ts
+++ src/utils/validations/suratRekomendasi.ts
@@ -43,7 +43,7 @@ export function validateNIM(value: unknown): ValidationResult {
 export function validateEmail(value: unknown): ValidationResult {
     const errors: string[] = [];
     const emailStr = typeof value === "string" ? value : String(value);
-    const emailRegex = /^[\w-.]+@[\w-]+\.[a-zA-Z]{2,}$/;
+    const emailRegex = /^[\w-.]+@([\w-]+\.)+[a-zA-Z]{2,}$/;
     if (!emailRegex.test(emailStr)) {
         errors.push("Format email tidak valid");
     }

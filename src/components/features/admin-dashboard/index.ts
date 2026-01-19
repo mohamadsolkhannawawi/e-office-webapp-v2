a50eb20613eb1b120e72cbac8753769a689a631b
MohamadSolkhanNawawi <mohamadsolkhan558@gmail.com>
2026-01-19 19:04:14 +0700

    refactor(components): remove legacy feature components including admin dashboards and old detail views

--- src/components/features/admin-dashboard/index.ts
+++
@@ -1,2 +0,0 @@
-export * from "./AdminDashboard";
-export * from "./LetterList";
--- src/components/features/detail-surat/index.ts
+++
@@ -1,5 +0,0 @@
-export * from "./IdentitasPengaju";
-export * from "./DetailSuratPengajuan";
-export * from "./LampiranSurat";
-export * from "./RiwayatSurat";
-export * from "./DetailRevisi";

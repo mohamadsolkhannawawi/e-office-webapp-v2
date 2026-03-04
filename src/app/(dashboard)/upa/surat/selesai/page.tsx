b6139ced2031f7283487c5251fb7fe7339975e39
setaDewaB <setadewab@gmail.com>
2026-03-05 06:57:00 +0700

    fix(upa): update arsip and selesai surat pages

--- src/app/(dashboard)/upa/surat/selesai/page.tsx
+++ src/app/(dashboard)/upa/surat/selesai/page.tsx
@@ -220,7 +220,7 @@ export default function SelesaiPage() {
                                 <SelectItem value="internal">
                                     Beasiswa Internal
                                 </SelectItem>
-                                <SelectItem value="external">
+                                <SelectItem value="eksternal">
                                     Beasiswa Eksternal
                                 </SelectItem>
                                 <SelectItem value="akademik">
